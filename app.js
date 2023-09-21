592a61f54f53b5669ccc1f8068ea27068efd7d9f
Sebastian Giron Valdizan <sebastiangironvaldizan@MacBook-Air-de-Sebastian.local>
2023-09-20 20:18:10 -0500

    ejercicio 1

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("Números par")
+for(let i = 1; i <= 100; i++){
+    if(i % 2 == 0 ? console.log(i) : "No es par");
+}
+console.log("Multiplos de 5");
+for(let i = 1; i <= 100; i++){
+    if(i % 5 == 0 ? console.log(i) : "No es multiplo de 5");
+}
